bc3721e59b4750ea8087e51746629b518c8ea993
Suman Saha <8759127658itisaha@gmail.com>
2026-05-30 16:33:14 +0530

    feat: initialize root layout with global styles, SEO metadata, and analytics integration

--- src/app/layout.js
+++ src/app/layout.js
@@ -49,14 +49,6 @@ export default function RootLayout({ children }) {
             alt=""
           />
         </noscript>
-        {/* Google Tag Manager */}
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-P4X2NRMR');`}
-        </Script>
       </head>
       <body className="min-h-full flex flex-col">
         {/* Google Tag Manager (noscript) */}
@@ -68,6 +60,14 @@ export default function RootLayout({ children }) {
             style={{ display: "none", visibility: "hidden" }}
           />
         </noscript>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-P4X2NRMR');`}
+        </Script>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-Q80WWE9ZSD"
           strategy="afterInteractive"
